e00bd2c169c69512026866f705c9a775c8faa51d
Julian Foo <julianfssen@gmail.com>
2020-06-21 08:53:45 -0400

    Added logic to filter destinations by region

--- destinations/views.py
+++ destinations/views.py
@@ -8,4 +8,16 @@ def home(request):
 
 class DestinationListView(ListView):
 	model = Destination
+	fields = ['region']
+	template_name = 'destinations/destination_list.html'
 	paginate_by = 10
+
+	def get_queryset(self):
+		region = self.request.GET.get('region', 'asia')
+		context = Destination.objects.filter(region__iexact = region)
+		return context
+
+	def get_context_data(self, **kwargs):
+		context = super(DestinationListView, self).get_context_data(**kwargs)
+		context['region'] = self.request.GET.get('region', 'asia')
+		return context
